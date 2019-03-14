43e2a6055ce881c5a800f79bff156da0de7724fe
Neology92 <oskarlegner@gmail.com>
2019-03-14 14:55:02 +0100

    points system implemented in game

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
     return 1;
   }
 
-
+  Points points;  // Deklaracja obiektu podliczającego punkty użytkownika
   
   std::cout << std::endl;
   std::cout << " Start testu arytmetyki zespolonej: " << argv[1] << std::endl;
@@ -32,18 +32,21 @@ int main(int argc, char **argv)
   Expression   expression;
   
   while (GetNextQuestion(&base,&expression)) {
-    Complex complex{0,0};
+    Complex task{0,0};
+    Complex answer{0,0};
     
     std::cout << "Podaj wynik operacji: ";
     std::cout << expression << " = ";
 
-    std::cin >> complex;
-    std::cout << complex << std::endl;
+    std::cin >> answer;
+    task = Solve(expression);
+    points.check(task, answer);
   }
-
   
   std::cout << std::endl;
   std::cout << " Koniec testu" << std::endl;
   std::cout << std::endl;
+
+  points.showResults();
 
 }
